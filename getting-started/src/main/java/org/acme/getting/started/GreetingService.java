ab6e42147a39a55a36298357218161c02c3fc91c
Martin Glass <marting@morinda.com>
2021-02-12 12:39:13 -0700

    Add output of interfaces implemented on GreetingTest

--- getting-started/src/main/java/org/acme/getting/started/GreetingService.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingService.java
@@ -8,6 +8,11 @@ public class GreetingService {
     public String greeting(String name) {
         GreetingTest test = new GreetingTest();
         test.setName(name);
+        // check class interfaces
+        System.out.println("GreetingTest implemented interfaces: "+ GreetingTest.class.getInterfaces().length);
+        for (Class interfaceclass : GreetingTest.class.getInterfaces()) {
+            System.out.println("GreetingTest implemented interfaces: "+ interfaceclass.getName());
+        }
         return "hello " + name;
     }
 
